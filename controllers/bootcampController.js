3e4b2404ae41e1c63602bbd3d300c523a718cd43
abkhan01 <124438296+abkhan01@users.noreply.github.com>
2023-02-09 21:57:04 +0530

    added mongoose

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -1,5 +1,13 @@
+const user = require('../models/User');
+
+const mapResult = (data) => {
+    return data.map(({ userName, email }) => ({ userName, email }));
+};
+
 const getAllBootCamps = (req, res, next) => {
-    res.status(200).json({ success: true, data: [] })
+    user.find({}).then(data => {
+        res.status(200).json({ success: true, data: mapResult(data) })
+    })
 }
 
 module.exports = { getAllBootCamps };
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+var bodyParser = require("body-parser");
 
 dotenv.config({ path: './config/config.env' });
 
-console.log(process.env.PORT);
+console.log(`Trying to connect to ${process.env.MONGO}`)
+mongoose.connect(process.env.MONGO).then(() => console.log(`Connected Succefully`)).catch(err => console.log(err));
+
 
 const bootcamps = require('./routes/bootcamp');
+const User = require('./models/User');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use("/api/v1/bootcamp", bootcamps);
+
+
 
 app.listen(process.env.PORT, console.log(`server is listening to ${process.env.PORT} in ${process.env.NODE_ENV} mode`));
